d1c629c3a24138be9a8f2ca4c2d262b8607911ba
Jack-Cb <loreleijack2004@gmail.com>
2026-05-11 18:47:48 -0700

    Minor code staging

--- Code/sensOrbPN7160/STM32PN7160/src/main.cpp
+++ Code/sensOrbPN7160/STM32PN7160/src/main.cpp
@@ -1,3 +1,4 @@
+// Retrieved from send NDEF prototype: configure READ functionality and swap alternate functionality
 
 #include "Electroniccats_PN7150.h"
 #include <Arduino.h>
@@ -25,15 +26,7 @@ char acc_io[1000];
 
 TCA9548A I2CMux;
 
-// For SPI mode, we need a CS pin
-//#define BNO08X_CS 10
-//#define BNO08X_INT 9
-
-// For SPI mode, we also need a RESET
-//#define BNO08X_RESET 5
-// but not for I2C or UART
 #define BNO08X_RESET -1
-
 Adafruit_BNO08x bno08x(BNO08X_RESET);
 sh2_SensorValue_t sensorValue;
 void setReports(void);
@@ -41,16 +34,22 @@ void setReports(void);
 #define PN7160_IRQ (PA3)
 #define PN7160_VEN (PA4)
 #define PN7160_ADDR (0x28)
+bool current_mode = 1; // 1 - Write mode; 0 - Read mode
 
-// Function prototypes
+// Function prototypes send function
 void messageSentCallback();
 
-Electroniccats_PN7150 nfc(PN7160_IRQ, PN7160_VEN, PN7160_ADDR, PN7160); // creates a global NFC device interface object, attached to pins 7 (IRQ) and 8 (VEN) and using the default I2C address 0x28,specify PN7150 or PN7160 in constructor
+// Function prototypes read functions
+void messageReceivedCallback();
+String getHexRepresentation(const byte *data, const uint32_t dataSize);
+void displayDeviceInfo();
+void displayRecordInfo(NdefRecord record);
 
+Electroniccats_PN7150 nfc(PN7160_IRQ, PN7160_VEN, PN7160_ADDR, PN7160); // creates a global NFC device interface object, attached to pins 7 (IRQ) and 8 (VEN) and using the default I2C address 0x28,specify PN7150 or PN7160 in constructor
 NdefMessage message;
 
 // Three records, "Hello", "world" and Uri "https://www.electroniccats.com"
-const char ndefMessage[] =        {0x91,                                                                                       // MB/ME/CF/1/IL/TNF
+const char dummyNdefMessage[] =        {0x91,                                                                                       // MB/ME/CF/1/IL/TNF
                             0x01,                                                                                       // Type length (1 byte)
                             0x08,                                                                                       // Payload length
                             'T',                                                                                        // Type -> 'T' for text, 'U' for URI
@@ -87,8 +86,6 @@ void setup() {
 
   // Try to initialize!
   if (!bno08x.begin_I2C()) {
-    // if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte
-    // UART buffer! if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
     Serial.println("Failed to find BNO08x chip");
     while (1) {
       delay(10);
@@ -236,11 +233,6 @@ void loop() {
                             io[36], io[37], io[38], io[39], io[40],
                             io[41], io[42], io[43], io[44], io[45],
                             io[46], io[47], io[48], io[49], io[50]};                                                                                    
-    
-    /*const char newdefMessage[] =        {0xD1,
-      0x01, 0x15, 0x55, 0x00, io[0], io[1], io[2], io[3], io[4],
-      io[5], io[6], io[7], io[8], io[9], io[10], io[11], io[12], io[13], io[14],
-      io[15], io[16], io[17], io[19], io[20]};        */
     message.setContent((newdefMessage), sizeof(newdefMessage));
     nfc.sendMessage();
     Serial.print("\nWaiting for an NDEF device");
@@ -260,4 +252,194 @@ void setReports(void) {
   if (!bno08x.enableReport(SH2_GYROSCOPE_CALIBRATED)) {
     Serial.println("Could not enable gyroscope");
   }
+}
+
+/// @brief Callback function called when an NDEF message is received
+void messageReceivedCallback() {
+  NdefRecord record;
+  Serial.println("Processing Callback...");
+
+  // message is automatically updated when a new NDEF message is received
+  // only if we call message.begin() in setup()
+  if (message.isEmpty()) {
+    Serial.println("--- Provisioned buffer size too small or NDEF message empty");
+    return;
+  }
+
+  Serial.print("NDEF message: ");
+  Serial.println(getHexRepresentation(message.getContent(), message.getContentLength()));
+
+  // Show NDEF message details, it is composed of records
+  do {
+    record.create(message.getRecord());  // Get a new record every time we call getRecord()
+    displayRecordInfo(record);
+  } while (record.isNotEmpty());
+}
+
+String getHexRepresentation(const byte *data, const uint32_t dataSize) {
+  String hexString;
+
+  if (dataSize == 0) {
+    hexString = "null";
+  }
+
+  for (uint32_t index = 0; index < dataSize; index++) {
+    if (data[index] <= 0xF)
+      hexString += "0";
+    String hexValue = String(data[index] & 0xFF, HEX);
+    hexValue.toUpperCase();
+    hexString += hexValue;
+    if ((dataSize > 1) && (index != dataSize - 1)) {
+      hexString += ":";
+    }
+  }
+  return hexString;
+}
+
+void displayDeviceInfo() {
+  Serial.println();
+  Serial.print("Interface: ");
+  switch (nfc.remoteDevice.getInterface()) {
+    case nfc.interface.ISODEP:
+      Serial.println("ISO-DEP");
+      break;
+    case nfc.interface.NFCDEP:
+      Serial.println("NFC-DEP");
+      break;
+    case nfc.interface.TAGCMD:
+      Serial.println("TAG");
+      break;
+    case nfc.interface.FRAME:
+      Serial.println("FRAME");
+      break;
+    case nfc.interface.UNDETERMINED:
+      Serial.println("UNDETERMINED");
+      break;
+    default:
+      Serial.println("UNKNOWN");
+      break;
+  }
+
+  Serial.print("Protocol: ");
+  switch (nfc.remoteDevice.getProtocol()) {
+    case nfc.protocol.UNDETERMINED:
+      Serial.println("UNDETERMINED");
+      break;
+    case nfc.protocol.T1T:
+      Serial.println("T1T");
+      break;
+    case nfc.protocol.T2T:
+      Serial.println("T2T");
+      break;
+    case nfc.protocol.T3T:
+      Serial.println("T3T");
+      break;
+    case nfc.protocol.ISODEP:
+      Serial.println("ISO-DEP");
+      break;
+    case nfc.protocol.NFCDEP:
+      Serial.println("NFC-DEP");
+      break;
+    case nfc.protocol.ISO15693:
+      Serial.println("ISO15693");
+      break;
+    case nfc.protocol.MIFARE:
+      Serial.println("MIFARE");
+      break;
+    default:
+      Serial.println("UNKNOWN");
+      break;
+  }
+
+  Serial.print("Technology: ");
+  switch (nfc.remoteDevice.getModeTech()) {
+    case nfc.modeTech.POLL | nfc.tech.PASSIVE_NFCA:
+      Serial.println("PASSIVE NFC A");
+      break;
+    case nfc.modeTech.POLL | nfc.tech.PASSIVE_NFCB:
+      Serial.println("PASSIVE NFC B");
+      break;
+    case nfc.modeTech.POLL | nfc.tech.PASSIVE_NFCF:
+      Serial.println("PASSIVE NFC F");
+      break;
+    case nfc.modeTech.POLL | nfc.tech.PASSIVE_15693:
+      Serial.println("PASSIVE 15693");
+      break;
+  }
+}
+
+void displayRecordInfo(NdefRecord record) {
+  if (record.isEmpty()) {
+    Serial.println("No more records, exiting...");
+    return;
+  }
+
+  uint8_t *payload = record.getPayload();
+  Serial.println("--- NDEF record received:");
+
+  switch (record.getType()) {
+    case record.type.MEDIA_VCARD:
+      Serial.println("vCard:");
+      Serial.println(record.getVCardContent());
+      break;
+
+    case record.type.WELL_KNOWN_SIMPLE_TEXT:
+      Serial.println("\tWell known simple text");
+      Serial.println("\t- Text record: " + record.getText());
+      break;
+
+    case record.type.WELL_KNOWN_SIMPLE_URI:
+      Serial.println("\tWell known simple URI");
+      Serial.print("\t- URI record: ");
+      Serial.println(record.getUri());
+      break;
+
+    case record.type.MEDIA_HANDOVER_WIFI:
+      Serial.println("\tReceived WIFI credentials:");
+      Serial.println("\t- SSID: " + record.getWiFiSSID());
+      Serial.println("\t- Network key: " + record.getWiFiPassword());
+      Serial.println("\t- Authentication type: " + record.getWiFiAuthenticationType());
+      Serial.println("\t- Encryption type: " + record.getWiFiEncryptionType());
+      break;
+
+    case record.type.WELL_KNOWN_HANDOVER_SELECT:
+      Serial.print("\tHandover select version: ");
+      Serial.print(*payload >> 4);
+      Serial.print(".");
+      Serial.println(*payload & 0xF);
+      break;
+
+    case record.type.WELL_KNOWN_HANDOVER_REQUEST:
+      Serial.print("\tHandover request version: ");
+      Serial.print(*payload >> 4);
+      Serial.print(".");
+      Serial.println(*payload & 0xF);
+      break;
+
+    case record.type.MEDIA_HANDOVER_BT:
+      Serial.println("\tBluetooth handover");
+      Serial.println("\t- Bluetooth name: " + record.getBluetoothName());
+      Serial.println("\t- Bluetooth address: " + record.getBluetoothAddress());
+      break;
+
+    case record.type.MEDIA_HANDOVER_BLE:
+      Serial.print("\tBLE Handover");
+      Serial.println("\t- Payload size: " + String(record.getPayloadLength()) + " bytes");
+      Serial.print("\t- Payload = ");
+      Serial.println(getHexRepresentation(record.getPayload(), record.getPayloadLength()));
+      break;
+
+    case record.type.MEDIA_HANDOVER_BLE_SECURE:
+      Serial.print("\tBLE secure Handover");
+      Serial.println("\t- Payload size: " + String(record.getPayloadLength()) + " bytes");
+      Serial.print("\t- Payload = ");
+      Serial.println(getHexRepresentation(record.getPayload(), record.getPayloadLength()));
+      break;
+
+    default:
+      Serial.println("\tUnsupported NDEF record, cannot parse");
+      break;
+  }
+
+  Serial.println("");
 }
